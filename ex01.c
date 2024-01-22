7bf6439e2047c608e7854d292b48a504904559a1
m4rt1naa <157366478+m4rt1naa@users.noreply.github.com>
2024-01-22 16:00:08 +0000

    d

---
+++ ex01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+ int variabile6;
+ printf("inserisci un numero\n");
+ scanf(" %d", &variabile6);
+ printf()
+ int variabile4;
+ printf("inserisci un altro numero\n");
+ scanf(" %d", &variabile4);
+printf("variabile6= %d\n faf", variabile6);
+
+
+    return(0);
+}
